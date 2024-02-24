34a6e82744b8bd6d60873b565a47e7d52f8237b3
Mahmoud.Hassan <mahmoud.hassan@g2k-group.com>
2024-02-25 00:12:30 +0200

    Adding google auth

---
+++ src/Backend/server.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const logger = require("morgan");
+const CustomError = require("./utils/customError");
+const globalError = require("./middlewares/errorMiddleware");
+
+dotenv.config({ path: "config.env" });
+const dbConnection = require("./config/database");
+
+const userRoute = require("./routes/userRoute");
+const googleAuthRouter = require("./routes/googleAuthRoute");
+const authRoute = require("./routes/authRoute");
+
+dbConnection();
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "uploads")));
+if (process.env.NODE_ENV === "development") app.use(logger("dev"));
+
+app.use(googleAuthRouter);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/auth", authRoute);
+
+app.use("/", (req, res, next) => {
+  console.log("sdd");
+  res.json({ res: "Hello!" });
+});
+
+app.all("*", (req, res, next) => {
+  const err = new CustomError(
+    `Can't find ${req.originalUrl} on this server`,
+    404
+  );
+  next(err);
+});
+
+app.use(globalError);
+
+const PORT = process.env.PORT || 8000;
+const server = app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(
+    `unhandeledRejection => name: ${err.name} | message: ${err.message}`
+  );
+  server.close(() => {
+    console.log("Shutting down server due to unhandled promise rejection");
+    process.exit(1);
+  });
+});
