e97e78ac925c811adb257aa9f8eda91d12cda52a
Mahmoud.Hassan <mahmoud.hassan@g2k-group.com>
2024-03-20 23:07:49 +0200

    adding article router

--- src/Backend/routes/articleRouter.js
+++ src/Backend/routes/articleRouter.js
@@ -1,3 +1,49 @@
 const express = require("express");
+const authService = require("../services/authService");
+const {
+  createArticleValidator,
+  getOrDeleteArticleByIdValidator,
+  updateArticleValidator,
+} = require("../utils/validators/articleValidator");
+const {
+  getArticles,
+  uploadArticleImage,
+  resizeArticleImages,
+  calculateReadingTime,
+  createArticle,
+  calculateFleschScore,
+  deleteArticleById,
+} = require("../services/articleService");
 
 const router = express.Router();
+
+router
+  .route("/")
+  .get(authService.allowTo("admin", "manager"), getArticles)
+  .post(
+    authService.allowTo("admin"),
+    createArticleValidator,
+    uploadArticleImage,
+    resizeArticleImages,
+    calculateReadingTime,
+    calculateFleschScore,
+    createArticle
+  );
+
+router
+  .route("/:id")
+  // .get(authService.allowTo("admin"), getOrDeleteArticleByIdValidator, getUserById)
+  .put(
+    authService.allowTo("admin"),
+    updateArticleValidator,
+    uploadArticleImage,
+    resizeArticleImages,
+    calculateReadingTime,
+    calculateFleschScore,
+    createArticle // TODO don't forget to change this route
+  )
+  .delete(
+    authService.allowTo("admin"),
+    getOrDeleteArticleByIdValidator,
+    deleteArticleById
+  );
